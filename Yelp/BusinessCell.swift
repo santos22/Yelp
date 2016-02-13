37e264dbfb6b2a116e2bd034e0ee9035e8633310
santos22 <santosjavier22@gmail.com>
2016-02-12 23:56:19 -0600

    Making progress with autolayout - no more overlapping

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -34,6 +34,15 @@ class BusinessCell: UITableViewCell {
         super.awakeFromNib()
         thumbImageView.layer.cornerRadius = 3
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // for when parent changes dimension
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
